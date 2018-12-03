40433a8f04b4747cd91123ede27791ae5de714b5
Grzegorz Skołyszewski <skolyszewski.grzegorz@gmail.com>
2018-12-03 23:19:37 +0100

    Setting up problem done and compiling, requires runtime error fix

--- example_post_handler.cc
+++ example_post_handler.cc
@@ -7,14 +7,26 @@ int main (int argc, const char *argv[])
     OsiClpSolverInterface *si = new OsiClpSolverInterface;
 
     // data goes here:
-    int RACK = 1; int SERV = 1;
-    int l1, L2[RACK], L3[SERV], l4;
-    int m1, m2, M2[SERV], m3, M3[RACK], m4;
-    int w1[RACK], w2, w3[SERV];
+
+    int l1 = 100;
+	int l2 = 2;
+	int l3 = 1;
+	int l4 = 5;
+	int L2[l2] = { 3000, 3200 };
+	int L3[l3] = { 10000 };
+    int m1 = 100000;
+	int m2 = 200000;
+	int M2[l2] = { 80000, 130000,  };
+	int m3 = 200000;
+	int M3[l3] = { 10000 };
+	int m4 = 65000000;
+    int w1[l3] = { 2 };
+	int w2 = 14;
+	int w3[l2] = { 400, 300 };
 
     // define objective and variable bounds
     int x1_cols = 1; int x4_cols = 1;
-    int n_cols = x1_cols + x4_cols + SERV + RACK;
+    int n_cols = x1_cols + x4_cols + l2 + l3;
     double * objective = new double[n_cols]; //the objective coefficients
     double * col_lb = new double[n_cols]; //the column lower bounds
     double * col_ub = new double[n_cols]; //the column upper bounds
@@ -22,19 +34,19 @@ int main (int argc, const char *argv[])
     // WE ALSO NEED TO KNOW x1, first x2, first x3 and x4 positions in objective row
     int x1_pos = 0;
     int x2_pos = 1;
-    int x3_pos = x2_pos + SERV;
-    int x4_pos = x3_pos + SERV;
+    int x3_pos = x2_pos + l2;
+    int x4_pos = x3_pos + l3;
 
     //minimize z: x1*l1 + (sum{serv in SERV} L2[serv]*x2[serv]) + (sum{rack in RACK} L3[rack]*x3[rack]) + x4*l4;
     objective[x1_pos] = l1;
-    for (int i = 0; i<SERV; i++) {
+    for (int i = 0; i<l2; i++) {
             objective[x2_pos+i] = L2[i];
     }
-    for (int i = 0; i<RACK; i++) {
+    for (int i = 0; i<l3; i++) {
             objective[x3_pos+i] = L3[i];
     }
     objective[x4_pos] = l4;
-
+    
     // variable bounds -> <0;inf>
     for (int i = 0; i<n_cols; i++) {
         col_lb[i] = 0.0;
@@ -42,7 +54,7 @@ int main (int argc, const char *argv[])
     }
 
     // constraints
-    int n_rows = x1_cols + x4_cols + SERV*3 + RACK*2 + SERV*RACK; // using sum/iterating 3 times on both RACK AND SERV
+    int n_rows = 6 + l2 + l3;
     double * row_lb = new double[n_rows]; //the row lower bounds
     double * row_ub = new double[n_rows]; //the row upper bounds
 
@@ -51,11 +63,15 @@ int main (int argc, const char *argv[])
 
     // Base vector with all zeros
     int indices[n_rows];
+    for (int i=0; i<n_rows; i++) {
+        indices[i] = i;
+    }
     const CoinPackedVector base_vector = new CoinPackedVector(n_rows, indices, 0.0);
-    int current_row = 0;
 
+    int current_row = 0;
     // constraint 1
     CoinPackedVector row1 = base_vector;
+    // WYWALA SIE NA LINICJE NIŻEJ SOMEHOW
     row1.setElement(x1_pos,1.0);
     row_lb[current_row] = 0;
     row_ub[current_row] = m1;
@@ -63,66 +79,50 @@ int main (int argc, const char *argv[])
     current_row++;
 
     // constraint 2
-    for (int i = 0; i<SERV; i++) {
-        CoinPackedVector row2 = base_vector;
-        // ADD ANOTHER FOR LOOP HERE:
+    CoinPackedVector row2 = base_vector;
+    for (int i = 0; i<l2; i++) {
         row2.setElement(x2_pos+i,1.0);
-        for (int j = 1; j<=SERV; j++) {
-            row2.setElement(x2_pos+((i+j)%SERV),-1.0);
-        }
-        row_lb[current_row] = m2;
-        row_ub[current_row] = si->getInfinity();
-        matrix->appendRow(row2);
-        current_row++;
     }
+    matrix->appendRow(row2);
+    row_lb[current_row] = m2;
+    row_ub[current_row] = si->getInfinity();
+    current_row++;
 
     // constraint 3
-    for (int i = 0; i<RACK; i++) {
-        for (int j = 0; j<SERV; j++) {
-            CoinPackedVector row5 = base_vector;
-            row5.setElement(x3_pos+i, w2); 
-            for (int k = 1; k<=RACK; k++) {
-                row5.setElement(x3_pos+((i+k)%RACK),-w2);
-            }
-            row5.setElement(x2_pos+j, -1.0);
-            for (int k = 1; k<=SERV; k++) {
-                row5.setElement(x3_pos+((j+k)%SERV),1.0);
-            }
-            row_lb[current_row] = 0;
-            row_ub[current_row] = si->getInfinity();
-            matrix->appendRow(row5);
-            current_row++;
-        }
+    CoinPackedVector row3 = base_vector;
+    for (int i = 0; i<l3; i++) {
+        row3.setElement(x3_pos+i, w2); 
     }
-
-
+    for (int j = 0; j<l2; j++) {
+        row3.setElement(x2_pos+j, -1.0);
+    }
+    row_lb[current_row] = 0;
+    row_ub[current_row] = si->getInfinity();
+    matrix->appendRow(row3);
+    current_row++;
+    
     // constraint 4
-    for (int i = 0; i<RACK; i++) {
-        CoinPackedVector row4;
+    CoinPackedVector row4;
+    for (int i = 0; i<l3; i++) {
         row4.setElement(x3_pos+i, w1[i]); 
-        for (int j = 1; j<=RACK; j++) {
-            row4.setElement(x3_pos+((i+j)%RACK),-w1[(i+j)%RACK]);
-        }
-        row_lb[current_row] = 0;
-        row_ub[current_row] = m1;
-        matrix->appendRow(row4);
-        current_row++;
     }
+    row_lb[current_row] = 0;
+    row_ub[current_row] = m1;
+    matrix->appendRow(row4);
+    current_row++;
 
     // constraint 5
-    for (int i = 0; i<SERV; i++) {
-        CoinPackedVector row5 = base_vector;
-        // set x4 as -1.0
-        row5.setElement(x4_pos,-1.0);
-        row5.setElement(x3_pos+i, w3[i]);
-        for (int j = 1; j<=SERV; j++) {
-            row5.setElement(x3_pos+((i+j)%SERV),-w3[(i+j)%SERV]);
-        }
-        row_lb[current_row] = 0;
-        row_ub[current_row] = 0;
-        matrix->appendRow(row5);
-        current_row++;
+    CoinPackedVector row5 = base_vector;
+    // set x4 as -1.0
+    row5.setElement(x4_pos,-1.0);
+    for (int i = 0; i<l2; i++) {
+        row5.setElement(x2_pos+i, w3[i]);
+
     }
+    row_lb[current_row] = 0;
+    row_ub[current_row] = 0;
+    matrix->appendRow(row5);
+    current_row++;
 
     // constraint 6
     CoinPackedVector row6 = base_vector;
@@ -133,9 +133,9 @@ int main (int argc, const char *argv[])
     current_row++;
 
     // constraint 7
-    for (int i = 0; i<SERV; i++) {
+    for (int i = 0; i<l2; i++) {
         CoinPackedVector row7 = base_vector;
-        row7.setElement(current_row+i,1.0);
+        row7.setElement(x2_pos+i,1.0);
         row_lb[current_row] = 0;
         row_ub[current_row] = M2[i];
         matrix->appendRow(row7);
@@ -143,9 +143,9 @@ int main (int argc, const char *argv[])
     }
 
     // constraint 8
-    for (int i = 0; i<RACK; i++) {
+    for (int i = 0; i<l3; i++) {
         CoinPackedVector row8 = base_vector;
-        row8.setElement(current_row+i,1.0);
+        row8.setElement(x3_pos+i,1.0);
         row_lb[current_row] = 0;
         row_ub[current_row] = M3[i];
         matrix->appendRow(row8);
